23e89d7b733d23d12c8b82d2665c0a9ccbea6e8c
Amanda <amandarb43@gmail.com>
2023-01-03 14:53:44 -0800

    masterupdate2

--- src/components/GalleryItem.js
+++ src/components/GalleryItem.js
@@ -1,11 +1,17 @@
 import { useState } from 'react'
 
-function GalleryItem(props) {
+function GalleryItem(props){
     let [view, setView] = useState(false)
 
     const simpleView = () => {
         return (
-            <div style={simpleStyle}>
+            <div style={{
+                'width': '25vw',
+                'height': '20vh',
+                'border': '1px solid black',
+                'margin' : '2px',
+                'position': 'relative'
+            }}>
                 <h3>{props.item.trackName}</h3>
                 <h4>{props.item.collectionName}</h4>
             </div>
@@ -14,7 +20,17 @@ function GalleryItem(props) {
 
     const detailView = () => {
         return (
-            <div style= {detailView}>
+            <div style={{
+                'width': '80vw',
+                'height': '20vh',
+                'border': '1px solid black',
+                'margin' : '2px',
+                'position': 'relative',
+                'backgroundImage': `url(${props.item.artworkUrl100})`,
+                'backgroundRepeat': 'no-repeat',
+                'backgroundSize': 'cover',
+                'color': 'white'
+            }}>
                 <h2>{props.item.trackName}</h2>
                 <h3>{props.item.collectionName}</h3>
                 <h4>{props.item.primaryGenreName}</h4>
@@ -26,30 +42,9 @@ function GalleryItem(props) {
     return (
         <div onClick={() => setView(!view)}
         style={{'display': 'inline-block'}}>
-        
-            {/* This simple ternary shows the simple view when 'view' is false! */}
             {view ? detailView() : simpleView()}
-
         </div>
     )
-
-}
-const simpleStyle = {
-    'width': '25vw',
-    'height': '20vh',
-    'border': '1px solid black',
-    'margin': '2px'
-}
-
-const detailStyle = {
-    'width': '80vw',
-    'height': '20vh',
-    'border': '1px solid black',
-    'margin': '2px',
-    'backgroundImage': `url(${props.item.artworkUrl100})`,
-    'backgroundRepeat': 'no-repeat',
-    'backgroundSize': 'cover',
-    'color': 'yellow'
-}
+} 
 
 export default GalleryItem
