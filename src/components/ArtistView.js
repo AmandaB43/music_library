85ab61c6796f91180685b66e1fbf0927b33c08c0
Amanda <amandarb43@gmail.com>
2023-01-03 14:24:44 -0800

    update2

--- src/components/ArtistView.js
+++ src/components/ArtistView.js
@@ -1,11 +1,10 @@
-import { useEffect, useState } from 'react'
-import { useParams, Link, useHistory } from 'react-router-dom'
-import Spinner from './Spinner'
+import {useEffect, useState} from 'react'
+import {useParams, Link, useHistory} from 'react-router-dom'
 
 const ArtistView = () => {
     const { id } = useParams()
-    const history = useHistory()
     const [ artistData, setArtistData ] = useState([])
+    const history = useHistory()
     
     useEffect(() => {
         const API_URL = `http://localhost:4000/album/${id}`
@@ -18,27 +17,26 @@ const ArtistView = () => {
     }, [id])
 
     const allAlbums = artistData.filter(entity => entity.collectionType === 'Album')
-    .map((album, i) => {
-        return (
-            <div key={i}>
-                <Link to={`/album/${album.collectionId}`}>
-                    <p>{album.collectionName}</p>
-                </Link>
-            </div>)
-        })
-
+                        .map((album, i) => { 
+                            return (
+                                <div key={i}>
+                                    <Link to={`/album/${album.collectionId}`}>
+                                        <p>{album.collectionName}</p>
+                                    </Link>
+                                </div>)
+                                })
+    
     const navButtons = () => {
         return (
             <div>
-                <button onClick={() => {history.push('/')}}>Home</button> |
-                <button onClick={() => {history.goBack()}}>Back</button>
+                <button onClick={() => {history.push('/')}}>Home</button> | <button onClick={() => history.goBack()}>Back</button>
             </div>
         )
     }
 
     return (
         <div>
-            {artistData.length > 0 ? <h2>{artistData[0].artistName}</h2> : <Spinner />}
+            {artistData.length > 0 ? <h2>{artistData[0].artistName}</h2> : <p>loading...</p>}
             {navButtons()}
             {allAlbums}
         </div>
